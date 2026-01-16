9d5f4de0531a0b318da51d33c040142a9b1f5060
tugalsan karabacak <tugalsan@users.noreply.github.com>
2026-01-16 13:47:12 +0300

    .

--- com.tugalsan.java.app.paint/com.tugalsan.java.app.paint-shared/src/main/java/module-info.java
+++ com.tugalsan.java.app.paint/com.tugalsan.java.app.paint-shared/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.tugalsan.java.app.paint.shared {
-    requires gwt.servlet.jakarta;
+//    requires gwt.servlet.jakarta;
+    requires gwt.user;
     requires com.tugalsan.java.core.servlet.gwt.webapp;
     requires com.tugalsan.java.lib.rql;
     exports com.tugalsan.java.app.paint.sg;
